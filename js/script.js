13c5a7bc41bf932cd11302cd9e831b0f0feb17ed
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-28 00:34:22 +0530

    added init game function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const wordText = document.querySelector(".word");
+hintText = document.querySelector(".hint span");
+
+const initGame = () => {
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  let wordArray = randomObj.word.split("");
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+  }
+  wordText.innerText = wordArray.join("");
+  hintText.innerText = randomObj.hint;
+  console.log(wordArray, randomObj.word);
+};
+initGame();
---
+++ js/words.js
@@ -0,0 +1,50 @@
+const words = [
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "A person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "one-sixteith of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+  {
+    word: "addition",
+    hint: "The process of adding numbers",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "A piece of fabric for painting",
+  },
+  {
+    word: "garden",
+    hint: "A plac for planting flower and plants",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+];
