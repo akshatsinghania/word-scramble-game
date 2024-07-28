9c03fcebbe517f043712be508447b715ce52a2db
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-29 00:48:33 +0530

    completed the app

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 const wordText = document.querySelector(".word");
 const hintText = document.querySelector(".hint span");
+const timeText = document.querySelector(".time b");
 const inputField = document.querySelector("input");
 const refreshBtn = document.querySelector(".refresh-word");
 const checkBtn = document.querySelector(".check-word");
 
-let correctWord;
-// Assuming words is an array of objects like this
-// const words = [{word: "example", hint: "This is a sample word"}];
+let correctWord, timer;
+
+const initTimer = (maxTime) => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innerText = maxTime);
+    }
+    clearInterval(timer);
+    alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+    initGame();
+  }, 1000);
+};
 
 const initGame = () => {
+  initTimer(30);
   let randomObj = words[Math.floor(Math.random() * words.length)];
   let wordArray = randomObj.word.split("");
   for (let i = wordArray.length - 1; i > 0; i--) {
@@ -18,15 +31,19 @@ const initGame = () => {
   wordText.innerText = wordArray.join("");
   hintText.innerText = randomObj.hint;
   correctWord = randomObj.word.toLowerCase();
+  inputField.value = "";
+  inputField.setAttribute("maxlength", correctWord.length);
   console.log(wordArray, randomObj.word);
 };
 initGame();
 
 const checkWord = () => {
   let userWord = inputField.value.toLowerCase();
+  if (!userWord) return alert(`Please enter a word check`);
   if (userWord !== correctWord)
     return alert(`Oops! ${userWord} is not a correct word`);
   alert(`Congrats! ${userWord.toUpperCase()} is a correct word`);
+  initGame();
 };
 
 refreshBtn.addEventListener("click", initGame);
